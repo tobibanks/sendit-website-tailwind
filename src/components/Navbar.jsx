1589959e3db228887dddf43c4344153fffbccae0
tobibanks <tobiodogwu@gmail.com>
2024-02-17 16:32:20 +0100

    omo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
         <li className='p-4'>Contact</li>
       </ul>
       <div onClick={handleNav} className='block md:hidden'>
-          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
+          {nav ? < AiOutlineMenu size={20}/> : <AiOutlineClose size={20} />}
       </div>
       <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
         <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
